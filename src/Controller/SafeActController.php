47c59e1acff5ee46f0561ff3dbb37904a51f78a9
hamhoum10 <omriyasser12@gmail.com>
2023-07-06 18:00:48 +0100

    homme page

--- src/Controller/SafeActController.php
+++ src/Controller/SafeActController.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/safeactenergie')]
 class SafeActController extends AbstractController
 {
-    #[Route('/homme', name: 'app_safe_act')]
-    public function index1(): Response
+    #[Route('/contact_us', name: 'app_safe_act')]
+    public function contact_us(): Response
     {
-        return $this->render('safe_act/index.html.twig');
+        return $this->render('safe_act/contact_us.html.twig');
     }
+    #[Route('/homme', name: 'app_safeact_homme_page')]
+    public  function homme_page():Response
+    {
+        return $this -> render('safe_act/homme.html.twig');
+    }
+
 }
